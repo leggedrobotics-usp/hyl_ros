2c718dd5e2e5f6fab71a872c6f7985c524cd2563
Victor T. N <victortn21@hotmail.com>
2024-08-20 13:07:58 +0000

    Remove unnecessary imports
    Signed-off-by: Victor T. N <victortn21@hotmail.com>

--- hyl_control/launch/position_control.launch.py
+++ hyl_control/launch/position_control.launch.py
@@ -4,8 +4,6 @@ from ament_index_python.packages import get_package_share_directory
 from ament_index_python.packages import get_package_prefix
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
